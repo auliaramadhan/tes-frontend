218bea841fc0f9ce05996ad6409d1f73a05c98d9
auliaramadhan <aul.dark@gmail.com>
2020-03-04 14:18:33 +0700

    sudah loading

--- js/index.js
+++ js/index.js
@@ -5,19 +5,59 @@ const header = {
   }
 }
 
+const logError = err => console.log(err)
+
 var store = new Vuex.Store({
   state: {
-    count: 0
-  },
-  mutations: {
-    increment: function (state) {
-      state.count++
-    }
+    todos: []
   },
   getters: {
-    counter: function (state) {
-      return state.count
-    }
+    getTodos: function (state) {
+      return state.todos
+    },
+  },
+  mutations: {
+    setTodos: function (state, todos) {
+      state.todos = todos
+    },
+    addTodo: function (state, todo) {
+      state.todos.unshift(todo)
+    },
+    removeTodo: function (state, todo) {
+      state.todos = state.todos.filter(i =>  i.id !== todo)
+    },
+    completedTodo: function (state, todo) {
+      const index = state.todos.findIndex(e => e.id === todo.id)
+      state.todos[index].completed = !state.todos[index].completed
+    },
+  },
+  actions: {
+    fetchTodos({ commit }) {
+      axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(response => {
+          commit('setTodos', response.data)
+        }).catch(logError)
+    },
+    async postTodo({ commit }, todo) {
+      await axios.post('https://jsonplaceholder.typicode.com/todos', todo, header)
+        .then(response => {
+          console.log(response.data)
+          commit('addTodo', response.data)
+        })
+        .catch(logError)
+    },
+    async deleteTodo({ commit }, id) {
+      await axios.delete('https://jsonplaceholder.typicode.com/posts/' + id, header)
+        .then(response => {
+          console.log(response)
+          commit('removeTodo', id)
+        }).catch(logError)
+    },
+    async patchTodo({ commit }, id) {
+      await axios.patch('https://jsonplaceholder.typicode.com/todos/' + id, { completed: true }, header)
+        .then(response => commit('completedTodo', response.data))
+        .catch(logError)
+    },
   }
 })
 
@@ -32,61 +72,42 @@ var app = new Vue({
     }
   },
   created() {
+    console.log(store)
     this.getTodos();
   },
-  // watch: {
-  //   todos() {
-  //     console.log('todo', this.todos)
-  //     localStorage.setItem("todos", JSON.stringify(this.todos));
-  //   }
-  // },
+  computed: {
+    todos: function () {
+      return store.getters.getTodos
+    },
+  },
   methods: {
     getTodos() {
-      axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then(response => {
-          this.todos = response.data
-        })
-        .catch(error => {
-          console.log(error);
-        })
+      store.dispatch('fetchTodos');
     },
-    postTodo() {
+    async postTodo() {
+      if (this.todo === '') {
+        M.toast({html: 'Must fill data!', displayLength: 3000})
+        return;
+      }
       const todo = {
         userId: 11,
         title: this.todo,
         completed: false
       }
-      console.log('hai', todo)
       this.isLoading = true;
-      axios.post('https://jsonplaceholder.typicode.com/todos', todo,header)
-        .then(response => {
-          this.todos.push(response.data)
-          this.isLoading = false;
-          console.log(response)
-        })
-        .catch(error => {
-          this.isLoading = false;
-          console.log(error);
-        })
+      await store.dispatch('postTodo', todo)
+      this.todo = ''
+      this.isLoading = false;
     },
-    completedTodo(todo) {
-      console.log('hai', todo)
-      axios.patch('https://jsonplaceholder.typicode.com/todos/' + todo.id, { completed: true },header)
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
+    async completedTodo(todo) {
+      this.isLoading = true;
+      await store.dispatch('patchTodo', todo.id)
+      this.isLoading = false;
     },
-    deleteTodo(id) {
-      axios.delete('https://jsonplaceholder.typicode.com/posts/' + id,header)
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
+    async deleteTodo(id) {
+      this.isLoading = true;
+      await store.dispatch('deleteTodo', id)
+      this.isLoading = false;
     }
   }
 })
